590aa5d819b8535e18f8806133591266bbc057c0
Jonathan Roberts <95726437+JRoberts21@users.noreply.github.com>
2023-12-03 23:08:18 -0500

    added quantum console and relay

--- Assets/NetworkManagerUI.cs
+++ Assets/NetworkManagerUI.cs
@@ -3,19 +3,40 @@ using System.Collections.Generic;
 using Unity.Netcode;
 using UnityEngine;
 using UnityEngine.UI;
+using Unity.Netcode.Transports.UTP;
+using Unity.Networking.Transport.Relay;
+using Unity.Services.Authentication;
+using Unity.Services.Core;
+using Unity.Services.Relay;
+using Unity.Services.Relay.Models;
+
 
 public class NetworkManagerUI : MonoBehaviour
 {
+
    [SerializeField] private Button hostBtn;
    [SerializeField] private Button clientBtn;
 
-   private void Awake() {
+    private async void Start()
+    {
+        await UnityServices.InitializeAsync();
+
+        AuthenticationService.Instance.SignedIn += () =>
+        {
+            Debug.Log("Signed in" + AuthenticationService.Instance.PlayerId);
+        };
+        await AuthenticationService.Instance.SignInAnonymouslyAsync();
+    }
+
+    private void Awake() {
     hostBtn.onClick.AddListener(() => {
         NetworkManager.Singleton.StartHost();
+
     });
     clientBtn.onClick.AddListener(() => {
         NetworkManager.Singleton.StartClient();
     });
    }
-   
+
+
 }
